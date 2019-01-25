783847bdc0e7c2c4a89c130d1e529f8f0fc2f060
Gregory <greghojob@gmail.com>
2019-01-25 14:53:55 +0800

    1. change log style

--- ble/src/main/kotlin/com/ns/greg/ble/internal/BleLogger.kt
+++ ble/src/main/kotlin/com/ns/greg/ble/internal/BleLogger.kt
@@ -2,34 +2,46 @@ package com.ns.greg.ble.internal
 
 import android.util.Log
 import com.ns.greg.ble.BuildConfig
-import com.ns.greg.library.fancy_logger.FancyLogger
-import com.ns.greg.library.fancy_logger.Printer
 
 /**
  * @author gregho
  * @since 2018/6/26
  */
-internal class BleLogger {
+class BleLogger {
 
-  companion object {
+  companion object Functions {
 
     private const val TAG = "BleLogger"
+    private const val LEFT_BRACKET = "["
+    private const val RIGHT_BRACKET = "]"
+    private const val SIGN = "-->"
+    private const val LINK = "└--"
     private var debug = false
 
+    @JvmStatic
     fun enableLogger() {
       debug = true
     }
 
+    @JvmStatic
+    @JvmOverloads
     fun log(
-      functionName: String,
+      className: String = "",
+      functionName: String = "",
       message: String = ""
     ) {
       if (BuildConfig.DEBUG && debug) {
-        if (functionName.isNotEmpty()) {
-          Log.d(TAG, "-> $functionName")
+        if (className.isNotEmpty()) {
+          Log.d(TAG, "$LEFT_BRACKET$className$RIGHT_BRACKET")
         }
 
-        Log.d(TAG, message)
+        if (functionName.isNotEmpty()) {
+          Log.d(TAG, "$SIGN $functionName")
+        }
+
+        if (message.isNotEmpty()) {
+          Log.d(TAG, "$LINK $message")
+        }
       }
     }
   }
